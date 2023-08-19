fb6ab37d72bc38daef348b8cab500c26eb2f8873
韦琪 <weiqi@qq.com>
2023-08-19 12:04:44 +0800

    更新 readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Aurora情绪树洞
+# Aurora情绪树洞 - 前端
 ---
 ## 项目介绍
 **Aurora情绪树洞**是面向大学生为其提供心理健康辅助和情绪宣泄的网站。一个辅助大学生心理健康的平台，舒服的页面设计以及良好的交互效果必不可少，本系统通过问卷调查以及走访的方式询问了诸多大学生以及心理老师对于此系统开发的意见，也在知网上搜寻了许多关于色彩对于不同心理疾病的影响。网站还使用了大量的动态交互效果，增强了与大学生之间互动性。
@@ -35,33 +35,33 @@
 
 ### 功能模块展示
 1. **登录模块**
-![alt](https://gitee.com/tkey20/aurora/raw/master/gif/login.gif)
+![alt](http://vikeya.com:3527/aurora/login.gif)
 
 1. **社区页以及主页**
  * 社区页可查看所有人发表过的动态
 
-![alt](https://gitee.com/tkey20/aurora/raw/master/gif/mainPage.gif)
+![alt](http://vikeya.com:3527/aurora/mainPage.gif)
 
 3. **推荐动态、书籍、电影模块**
   * 推荐模块会根据用户当天选择的心情以及发表的动态的变化而实时推荐。
   * 点击爱心可以收集当前用户的情绪对于推荐喜爱，下次对于当前情绪的用户会优先推送这些推荐内容。
   
-![alt](https://gitee.com/tkey20/aurora/raw/master/gif/recommand.gif)
+![alt](http://vikeya.com:3527/aurora/recommand.gif)
 
 1. **小目标打卡功能**
 * 完成目标获得激励人心的反馈，促进大学生有信心面对问题。
-![alt](https://gitee.com/tkey20/aurora/raw/master/gif/小目标.gif)
+![alt](http://vikeya.com:3527/aurora/小目标.gif)
 
 4. **发布动态功能**
 * 发布成功后会识别用户是消极的情绪还是开心的情绪，并采取对应措施。
 * 识别到积极情绪，推荐模块的动态页也会实时推荐相似情绪的动态，引起共鸣。
-  ![alt](https://gitee.com/tkey20/aurora/raw/master/gif/发布.gif)
+  ![alt](http://vikeya.com:3527/aurora/发布.gif)
 
 * 识别到消极情绪，提供治愈弹窗功能，系统会虚拟鼓励用户，通过不同的鼓励语录对用户产生不同的激励效果。
-  ![alt](/tkey20/aurora/raw/master/gif/%E6%B6%88%E6%9E%81%E5%8F%91%E5%B8%83.gif)
+  ![alt](http://vikeya.com:3527/aurora/%E6%B6%88%E6%9E%81%E5%8F%91%E5%B8%83.gif)
 
-5. **心愿模块**
-  ![alt](https://gitee.com/tkey20/aurora/raw/master/gif/wish.gif)
+1. **心愿模块**
+  ![alt](http://vikeya.com:3527/aurora/wish.gif)
 
-6. **个人信息设置模块**
-![alt](https://gitee.com/tkey20/aurora/raw/master/gif/user.gif)
+1. **个人信息设置模块**
+![alt](http://vikeya.com:3527/aurora/user.gif)
--- src/pages/login/login_swiper.vue
+++ src/pages/login/login_swiper.vue
@@ -3,29 +3,61 @@
     <header class="header-content">
       <el-row>
         <el-col :span="7" class="header-title">
-          <router-link to="/" class="name" :class="{active: headerColor}">Aurora</router-link>
+          <router-link to="/" class="name" :class="{ active: headerColor }"
+            >Aurora</router-link
+          >
           <i class="headerLine"></i>
         </el-col>
-        <el-col :span="8" class="publish-text" >
-          <router-link to="/" class="header-title" :class="{active: headerColor}">首页</router-link>
-          <router-link to="/" class="header-title" :class="{active: headerColor}">关于我们</router-link>
-          <router-link to="/" class="header-title" :class="{active: headerColor}">
+        <el-col :span="8" class="publish-text">
+          <a
+            href="https://vikeya.com/"
+            target="_blank"
+            class="header-title"
+            :class="{ active: headerColor }"
+          >
+            我的博客
+          </a>
+          <a
+            href="https://github.com/Tkey20"
+            target="_blank"
+            class="header-title"
+            :class="{ active: headerColor }"
+          >
+            关于我们
+          </a>
+
+          <a
+            href="https://github.com/Tkey20"
+            target="_blank"
+            class="header-title"
+            :class="{ active: headerColor }"
+          >
             <i class="iconfont icon-github"></i>
             GitHub
-          </router-link>
+          </a>
         </el-col>
       </el-row>
     </header>
     <div class="swiper-container" ref="swiper">
       <div class="swiper-wrapper">
         <div class="swiper-slide">
-          <img src="@assets/img/123.jpg" alt="">
+          <img src="@assets/img/123.jpg" alt="" />
           <div class="main-content">
             <div class="main-title">
-              <span class="ani" swiper-animate-effect="fadeInUp" swiper-animate-duration="1s">树洞 倾诉你的所有</span>
+              <span
+                class="ani"
+                swiper-animate-effect="fadeInUp"
+                swiper-animate-duration="1s"
+                >树洞 倾诉你的所有</span
+              >
             </div>
             <div class="main-desc">
-              <span class="ani" swiper-animate-effect="fadeInUp" swiper-animate-duration="1s">温暖 / 真实 / 自信</span>
+              <span
+                class="ani"
+                swiper-animate-effect="fadeInUp"
+                swiper-animate-duration="1s"
+                >温暖 / 真实 / 自信</span
+              >
             </div>
           </div>
         </div>
@@ -40,10 +72,13 @@
 </template>
 
 <script>
-import LoginPage from './login_.vue'
-import '@assets/styles/animate.css'
-import { swiperAnimateCache, swiperAnimate } from '@assets/styles/swiper.animate1.0.3'
-import Swiper from 'swiper'
+import LoginPage from "./login_.vue";
+import "@assets/styles/animate.css";
+import {
+  swiperAnimateCache,
+  swiperAnimate
+} from "@assets/styles/swiper.animate1.0.3";
+import Swiper from "swiper";
 import "swiper/css/swiper.css";
 export default {
   components: {
@@ -53,129 +88,129 @@ export default {
     return {
       headerColor: false,
       swiper: null
-    }
+    };
   },
-    mounted() {
+  mounted() {
     this.init();
   },
   methods: {
     init() {
-      let that = this
+      let that = this;
       this.swiper = new Swiper(this.$refs.swiper, {
         mousewheel: true,
-        direction: 'vertical',
+        direction: "vertical",
         allowTouchMove: false,
         pagination: {
           el: ".swiper-pagination"
         },
         on: {
           init: function() {
-            swiperAnimateCache(this); // 隐藏动画元素 
+            swiperAnimateCache(this); // 隐藏动画元素
             swiperAnimate(this); // 初始化完成开始动画
-          }, 
-          slideChangeTransitionEnd: function() { 
+          },
+          slideChangeTransitionEnd: function() {
             swiperAnimate(this); // 每个slide切换结束时也运行当前slide动画
             // this.slides.eq(this.activeIndex).find('.ani').removeClass('ani'); 动画只展现一次，去除ani类名
-            that.headerColor = !that.headerColor
+            that.headerColor = !that.headerColor;
           }
         }
       });
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
-.swiperWrapper{
+.swiperWrapper {
   height: 100%;
   position: relative;
   min-width: 1100px;
 }
-.swiperWrapper /deep/ .swiper-container{
+.swiperWrapper /deep/ .swiper-container {
   height: 100%;
 }
-img{
+img {
   width: 100%;
   height: 100%;
 }
-.header-content{
+.header-content {
   width: 100%;
   min-width: 1100px;
   position: fixed;
   height: 52px;
   z-index: 100;
   top: 0;
-  .el-row{
+  .el-row {
     height: 100%;
     display: flex;
     justify-content: space-between;
   }
-  .el-col{
+  .el-col {
     height: 100%;
     display: flex;
     align-items: center;
   }
-  .headerLine{
+  .headerLine {
     margin-left: 17px;
     height: 29px;
     width: 1px;
     background-color: #ebebeb;
   }
-  .publish-text{
+  .publish-text {
     margin-left: auto;
     // text-align: center;
   }
-  .header-title{
-    margin:0 25px; 
+  .header-title {
+    margin: 0 25px;
     font-size: 20px;
     color: #ffffff;
     font-weight: 600;
     font-synthesis: style;
     transition: color 0.5s;
-    &.active{
+    &.active {
       color: @text-333;
       transition: color 0.5s;
     }
-    i{
+    i {
       font-size: 20px;
     }
   }
-  .name{
-    margin:0 auto;
+  .name {
+    margin: 0 auto;
     cursor: pointer;
     font-size: 38px;
     color: rgb(255, 255, 255);
     text-shadow: 2px 2px #ccc;
     font-weight: 500;
     transition: color 0.5s;
-    &.active{
+    &.active {
       transition: color 0.5s;
       color: @text-333;
     }
   }
 }
-.main-content{
+.main-content {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   width: 902px;
   height: 220px;
-  .main-title{
+  .main-title {
     height: 144px;
     display: flex;
     justify-content: center;
     // font-family: Microsoft JhengHei;
     font-weight: bold;
-    span{
+    span {
       font-size: 90px;
       color: white;
     }
   }
-  .main-desc{
+  .main-desc {
     display: flex;
     justify-content: center;
-    span{
+    span {
       font-size: 23px;
       color: white;
     }
